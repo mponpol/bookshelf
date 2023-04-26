a2c79f6a53162a8bc0a372b356bc59c8233d9db1
Mario Ponce <mponpol@gmail.com>
2023-04-26 17:38:37 +0200

    Render Logo and Title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Logo } from './components/logo';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => alert('Login clicked')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => alert('Register clicked')}>Register</button>
+      </div>
+    </div>
   );
 }
 
